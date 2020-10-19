7a8fa5df97c29fa6a0ad1d84e5294808156ca7b7
Mark Hall <Mark.Hall@sas.com>
2020-10-19 11:26:05 -0400

    (#33) Added test schedule, ping test, variable validation.

--- ldap_validator/ldap_validator.py
+++ ldap_validator/ldap_validator.py
@@ -33,6 +33,18 @@ cluster_settings = None
 logFile = "ldap_validation_" + str(datetime.now()) + ".log"
 open(logFile, 'a').close()
 
+# sitedefault variables
+ldap_server_host = ""
+ldap_server_port = ""
+ldap_protocol = ""
+ldap_anon_bind = ""
+ldap_bind_userdn = ""
+ldap_bind_pw = ""
+ldap_user_basedn = ""
+ldap_group_basedn = ""
+ldap_defaultadmin_user = ""
+ldap_url = ""
+
 # --------------------------------------------------------------------------------------------------
 def logMessage(message, error_flag=False):
     """
@@ -90,15 +102,48 @@ def importConfigYAML(yaml_file):
 
         logMessage("Successfully loaded yaml file '" + str(yaml_file) + "'")
 
-        assert(yaml_content['config']['application']['sas.identities.providers.ldap.connection']['host'] is not None), "Error: LDAP Host is undefined."
-        assert(yaml_content['config']['application']['sas.identities.providers.ldap.connection']['port'] is not None), "Error: LDAP port is undefined."
-        assert(yaml_content['config']['application']['sas.identities.providers.ldap.connection']['url'] is not None), "Error: LDAP URL is undefined."
-        assert(yaml_content['config']['application']['sas.identities.providers.ldap.connection']['anonymousBind'] is not None), "Error: LDAP anonymousBind is undefined."
-        assert(yaml_content['config']['application']['sas.identities.providers.ldap.connection']['userDN'] is not None), "Error: LDAP userDN is undefined."
-        assert(yaml_content['config']['application']['sas.identities.providers.ldap.connection']['password'] is not None), "Error: LDAP password is undefined."
-        assert(yaml_content['config']['application']['sas.identities.providers.ldap.user']['baseDN'] is not None), "Error: LDAP User BaseDN is undefined."
-        assert(yaml_content['config']['application']['sas.identities.providers.ldap.group']['baseDN'] is not None), "Error: LDAP Group BaseDN is undefined."
-        assert(yaml_content['config']['application']['sas.identities']['administrator'] is not None), "Error: LDAP Administrator is undefined."
+        # define simpler variables
+        global ldap_server_host
+        global ldap_server_port
+        global ldap_protocol
+        global ldap_anon_bind
+        global ldap_bind_userdn
+        global ldap_bind_pw
+        global ldap_user_basedn
+        global ldap_group_basedn
+        global ldap_defaultadmin_user
+        global ldap_url
+        
+        ldap_url = yaml_content['config']['application']['sas.identities.providers.ldap.connection']['url']
+        ldap_server_host = yaml_content['config']['application']['sas.identities.providers.ldap.connection']['host']
+        ldap_server_port = yaml_content['config']['application']['sas.identities.providers.ldap.connection']['port']
+        ldap_anon_bind = yaml_content['config']['application']['sas.identities.providers.ldap.connection']['anonymousBind']
+        ldap_bind_pw = yaml_content['config']['application']['sas.identities.providers.ldap.connection']['password']
+        ldap_bind_userdn = yaml_content['config']['application']['sas.identities.providers.ldap.connection']['userDN']
+        ldap_user_basedn = yaml_content['config']['application']['sas.identities.providers.ldap.user']['baseDN']
+        ldap_group_basedn = yaml_content['config']['application']['sas.identities.providers.ldap.group']['baseDN']
+        ldap_defaultadmin_user = yaml_content['config']['application']['sas.identities']['administrator']
+        ldap_protocol = ldap_url.split(':')[0]
+
+        logMessage("LDAP URL:" + ldap_url)
+        logMessage("LDAP Protocol:" + ldap_protocol)
+        logMessage("LDAP ServerHost:" + ldap_server_host)
+        logMessage("LDAP Server Port:" + str(ldap_server_port))
+        logMessage("LDAP Anonymous Bind:" + ldap_anon_bind)
+        if (ldap_bind_pw is not None): logMessage("LDAP Anonymous Bind Password: SET")
+        logMessage("LDAP Anonymous Bind User DN:" + ldap_bind_userdn)
+        logMessage("LDAP User DN:" + ldap_user_basedn)
+        logMessage("LDAP Group DN:" + ldap_group_basedn)
+
+        assert(ldap_url is not None), "Error: LDAP Administrator is undefined."
+        assert(ldap_server_host is not None), "Error: LDAP Host is undefined."
+        assert(ldap_server_port > 0), "Error: LDAP port is in correctly defined."
+        assert(ldap_anon_bind is not None), "Error: LDAP anonymousBind is undefined."
+        assert(ldap_bind_pw is not None), "Error: LDAP password is undefined."
+        assert(ldap_bind_userdn is not None), "Error: LDAP bind userDN is undefined."
+        assert(ldap_user_basedn is not None), "Error: LDAP User BaseDN is undefined."
+        assert(ldap_group_basedn is not None), "Error: LDAP Group BaseDN is undefined."
+        assert(ldap_defaultadmin_user is not None), "Error: LDAP Administrator is undefined."
 
         logMessage("Sitedefault contains required values.")
 
@@ -110,18 +155,61 @@ def importConfigYAML(yaml_file):
 
     return yaml_content
 
+# --------------------------------------------------------------------------------------------------
+def runTestSchedule():
+    """
+    Run the list of scheduled LDAP Validation tests.
 
+    Argument:
+        None
+    Returns:
+        success flag(bool) - Success/Failure of test schedule
+    """
+    logMessage('===Entry {0}==='.format(inspect.stack()[0][3]))
+    
+    pingHost()
+    
+    return True
+
+# --------------------------------------------------------------------------------------------------
+def pingHost():
+    """
+    Ping the LDAP server on the specified connection.
+
+    Argument:
+        None
+    Returns:
+        success flag(bool) - Success/Failure of test schedule
+    """
+    logMessage('===Entry {0}==='.format(inspect.stack()[0][3]))
+
+    logMessage("Attempting to ping LDAP server host at " + ldap_server_host)
+    logMessage(">> " + "ping -c 1 " + ldap_server_host)
+    
+    response = os.system("ping -c 1 " + ldap_server_host)
+
+    if (int(response) == 0):
+        logMessage("Network Active")
+        return True
+    else:
+        logMessage("Network Error")
+
+    return False
+
+# --------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------
 ##################
 #     main()     #
 ##################
-
 
 def main(argv):
 
     """
     Implementation of the main script execution for the pre_check install command.
 
-    :param argv: The parameters passed to the script at execution.
+    Argument:
+        arguments(string) - The parameters passed to the script at execution.
     """
     logMessage('===Entry {0}==='.format(inspect.stack()[0][3]))
 
@@ -142,10 +230,13 @@ def main(argv):
             logMessage("Setting sitedefault to: " + arg)
             sitedefault_loc = arg
         else:
-            logMessage("Unrecognized command line option <"+ opt + " " + arg + ">")
+            logMessage("Unrecognized command line option <" + opt + " " + arg + ">")
 
     cluster_settings = importConfigYAML(sitedefault_loc)
 
+    runTestSchedule()
+
+# --------------------------------------------------------------------------------------------------
 ##################
 #     main()     #
 ##################
